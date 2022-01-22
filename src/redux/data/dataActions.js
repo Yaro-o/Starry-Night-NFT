0126b9a1541a1908ccd19a818fe999ae225fa404
Yaroo <42944920+Yaro-o@users.noreply.github.com>
2022-01-22 07:29:40 -0500

    Add files via upload

---
+++ src/redux/data/dataActions.js
@@ -0,0 +1,42 @@
+// log
+import store from "../store";
+
+const fetchDataRequest = () => {
+  return {
+    type: "CHECK_DATA_REQUEST",
+  };
+};
+
+const fetchDataSuccess = (payload) => {
+  return {
+    type: "CHECK_DATA_SUCCESS",
+    payload: payload,
+  };
+};
+
+const fetchDataFailed = (payload) => {
+  return {
+    type: "CHECK_DATA_FAILED",
+    payload: payload,
+  };
+};
+
+export const fetchData = () => {
+  return async (dispatch) => {
+    dispatch(fetchDataRequest());
+    try {
+      let numTokensMinted = await store
+        .getState()
+        .blockchain.smartContract.methods.numTokensMinted().call();
+
+      dispatch(
+        fetchDataSuccess({
+          numTokensMinted,
+        })
+      );
+    } catch (err) {
+      console.log(err);
+      dispatch(fetchDataFailed("Could not load data from contract."));
+    }
+  };
+};
