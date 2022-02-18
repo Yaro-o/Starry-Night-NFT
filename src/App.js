5b593824dae44a300e2e549b1963844010422c3b
Yaroo <42944920+Yaro-o@users.noreply.github.com>
2022-02-18 06:39:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -362,7 +362,7 @@ function App() {
                 <s.TextDescription
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
                 >
-                  Gas limit is set at 90,000 to ensure the mint does not fail.
+                  Gas limit is set at 125,000 to ensure the mint does not fail.
                 </s.TextDescription>
 
                 <s.TextDescription
